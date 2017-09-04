914e67d5456ccfa202f3606bde1a11a8d089082d
Kolibri <kolibri@the-kolibri.com>
2017-09-04 07:29:54 -0500

    ADD: Sass and Watch tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,18 @@
 // Kolibri Gulp File
+
+var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    watch = require('gulp-watch');
+
+
+gulp.task('sass' , function() {
+    gulp.src('./src/scss/*.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('./dist/css/'))
+}); // Sass
+
+gulp.task('watch' , function() {
+    gulp.watch('./src/scss/*.scss' , ['sass']);
+}); // Watch
+
+gulp.task('default' , ['sass' , 'watch']);
